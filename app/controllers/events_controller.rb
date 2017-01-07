bb64233efb910289491f37f5ab875211a4b11bf5
Sam S <SamS@tests-MacBook-Pro.local>
2017-01-07 08:21:38 -0600

    pulled data titles within a 20 mile radius of sf

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,9 +1,22 @@
 class EventsController < ApplicationController
   def index
-    @events = []
-    @events << Unirest.get("https://api.seatgeek.com/2/events/3539572?&client_id=NjQwNTEzMXwxNDgxNDkxODI1").body
-    render 'index.html.erb'
-  end
-end
+    @events =[]
+    @events = Unirest.get("https://api.seatgeek.com/2/events?datetime_utc.gt=2017-01-07&datetime_utc.lte=2017-01-30&geoip=104.18.37.48&range=20mi&client_id=NjQwNTEzMXwxNDgxNDkxODI1").body
+    @b = []
+    @a = @events["events"]
+    @b << @a
 
+    
+   render 'index.html.erb'
+   end
+  
+ 
+
+    def show
+    event_id = params[:eid]
+    @events   = Unirest.get("https://api.seatgeek.com/2/events/#{params[:event_id]}").body
+
+    render 'show.html.erb'
+end
+end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/' => 'events#index'
+  get  '/events' => 'events#index'
+   get '/events/:id' => 'events#show'
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
   get '/login' => 'sessions#new'
