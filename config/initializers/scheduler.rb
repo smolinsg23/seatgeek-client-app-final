35503400649b036c4e0ed4427aff881c7a6354ee
Sam S <SamS@tests-MacBook-Pro.local>
2017-01-27 21:07:25 -0600

    sched

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -1,6 +1,6 @@
    unless defined?(Rails::Console) || File.split($0).last == 'rake'
      s = Rufus::Scheduler.singleton
-     s.every '10d', :tag => 'main_process' do
+     s.every '1d', :tag => 'main_process' do
      Rails.logger.info "hello, it's #{Time.now}"
      Rails.logger.flush
      Bid.all.each do |bid| 
